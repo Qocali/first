267ffc426893bca9ab5d56dfe4968dc8b6e571c7
Qocali <babayevqocheli@gmail.com>
2021-12-10 11:36:26 +0400

    Add project files.

---
+++ first/Controllers/DetaliesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace first.Controllers
+{
+    public class DetaliesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Action()
+        {
+            return View();
+        }
+        public IActionResult Else()
+        {
+            return View();
+        }
+
+    }
+}
